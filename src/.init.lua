ad6075e070291be43b5e9b2718f068163d189073
keb <keb@sheev.net>
2025-09-29 20:39:01 -0400

    wip

--- src/.init.lua
+++ src/.init.lua
@@ -1,19 +1,20 @@
 require 'global'
+local _ = require 'lib.lume'
 local moon = require 'lib.fullmoon'
 local util = require 'util'
 
 local INTERVAL_REGEX = assert(re.compile([[^--interval=([0-9]+)$]]))
 local ROWS_REGEX = assert(re.compile([[^--rows=([0-9]+)$]]))
 local COLUMNS_REGEX = assert(re.compile([[^--columns=([0-9]+)$]]))
-local DIR_REGEX = assert(re.compile([[^(/.*)$]]))
+local DIR_REGEX = assert(re.compile([[^([/.].*)]])) -- match strings starting with `/`, `.` or `..`
 
-local IMAGE_DIR = ''
+local IMAGE_DIRS = {}
 local ROWS = 3
 local COLUMNS = 4
 local INTERVAL = 2
 local VIDEO_RATE=15
 
-for _, v in ipairs(arg) do
+for i, v in ipairs(arg) do
   local match
   local value
 
@@ -41,23 +42,36 @@ for _, v in ipairs(arg) do
   match, value = DIR_REGEX:search(v)
 
   if match then
-    IMAGE_DIR = value
+    value = _.trim(value)
+
+    if value == '' or value == nil then
+      goto continue
+    end
+
+    if value:sub(1, 1) == '.' then
+      -- relative path
+      value = path.join(unix.getcwd(), value)
+    end
+
+    table.insert(IMAGE_DIRS, value)
   end
 
   ::continue::
 end
 
-if not IMAGE_DIR or IMAGE_DIR == '' then
-  LogError('Must provide directory argument')
+if #IMAGE_DIRS < 1 then
+  LogError('Must provide directory')
   unix.exit(1)
 end
 
-if not path.isdir(IMAGE_DIR) then
-  LogError('Provided path is not a valid directory: ' .. IMAGE_DIR)
-  unix.exit(1)
-end
+for _, dir in ipairs(IMAGE_DIRS) do
+  if not path.isdir(dir) then
+    LogError('Provided path is not a valid directory: ' .. dir)
+    unix.exit(1)
+  end
 
-ProgramDirectory(IMAGE_DIR)
+  ProgramDirectory(dir)
+end
 
 moon.setTemplate({ '/view/', tmpl = 'fmt' })
 moon.setRoute('/', moon.serveIndex('/view/'))
@@ -71,7 +85,17 @@ moon.get('/', function (r)
 end)
 
 moon.get('/images', function (r)
-  local images = util.getImages(IMAGE_DIR)
+  local images = {}
+
+  -- note to self: so this works, but now this is a problem because the frontend uses relative paths
+  -- because of the way ProgramDirectory works
+  -- what if both folder /a and /b have files with the same names? /a/foo.jpg and /b/foo.jpg? /a/ will take precedence
+  -- but /b/foo.jpg will never be considered
+  for i, dir in ipairs(IMAGE_DIRS) do
+    local results = util.getImages(dir)
+    images = _.concat(images, results)
+  end
+
   return moon.serveContent('json', { images = images })
 end)
 
